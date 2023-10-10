96c5bc352eafe2f93b8cbfd4b227a8d859e1dbb8
Karam Almakdasi <karamalmakdasi@gmail.com>
2023-10-11 01:39:44 +0200

    #- DeviceTileControle inte klar

---
+++ ServiceApplikationnew/MVVM/Models/DeviceItem.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ServiceApplikationnew.MVVM.Models;
+
+public class DeviceItem
+{
+
+    public string? DeviceId { get; set; }
+    public string? Devicetype { get; set; }
+    public string? Placement { get; set; }
+    public bool IsActive { get; set; }
+    public string? Icon => SetIcon();
+
+    private string SetIcon()
+    {
+        return (Devicetype?.ToLower()) switch
+        {
+            "light" => "\uf0eb",
+            "fan" => "\ue004",
+            _ => "\uf2db"
+        };
+    }
+}
